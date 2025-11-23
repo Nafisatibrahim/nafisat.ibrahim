a02b95dce8e05ffe2f145cff189bc6657c0710d3
Nafisat Ibrahim <nafisat.l@outlook.com>
2025-11-22 19:31:10 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+"""
+This is the main application module for the project.
+"""
+
+# Import libraries
+import streamlit as st
+
+# Page configuration
+st.set_page_config(
+    page_title="Nafisat Ibrahim - Portfolio",
+    page_icon=":briefcase:",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+# Light/Dark mode colors (to add later)
+
+# 
